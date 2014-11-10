fb123166c430d32a7694e5cd00dc061ca17924a8
aredman <aredman314@gmail.com>
2014-11-10 09:38:24 -0500

    Iterates through many angles.

---
+++ findDate.m
@@ -0,0 +1,15 @@
+clear all; clc; close all; format long eng; more off; 
+addpath('helper_functions')
+
+for i = 0:15
+	for j = 0:20
+		for k = 0:20
+			theta_d = (i+(j/10))*pi
+			theta_a = theta_d + (k/10)*pi
+			Mars_Project
+			'--------------------------'
+		end
+	end
+end
+
+
