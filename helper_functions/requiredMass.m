1ec522db68c8623e6b2aae4fd2d791d92034733e
aredman <aredman314@gmail.com>
2014-11-10 22:41:13 -0500

    Changed amount of food.

--- helper_functions/requiredMass.m
+++ helper_functions/requiredMass.m
@@ -1,7 +1,7 @@
 %file to hold helper functions for script
 function result = requiredMass(astronautCount, days)
-	%Assume that 1.4 pounds of food needed per day, 7 pounds of water, and 2 pounds of oxygen
-	food_H20_O2_mass = days*astronautCount*(1.4 + 7 + 2)*0.45359237;
+	%Assume that 2.32 kg of food needed per day, 7 pounds of water, and 2 pounds of oxygen
+	food_H20_O2_mass = days*astronautCount*(2.32 + (7 + 2)*0.45359237);
 	astronauts_mass = astronautCount*70; %153 pounds per person (70kg)
 
 	result = food_H20_O2_mass + astronauts_mass;
