b22893189cf3cbf93a06dc01a848bda74a5755b3
aredman <aredman314@gmail.com>
2014-11-11 19:15:08 -0500

    Fixed bug where orbitals were calculated more than once and the simulation stopped before 15 years.

--- findDate.m
+++ findDate.m
@@ -6,7 +6,7 @@ file = fopen('file.txt', 'w')
 for i = 0:15
 	for j = 0:20
 		for k = 0:20
-			theta_d = (i+(j/10.0))*pi
+			theta_d = (2*i)*pi + (j/10.0)*pi
 			theta_a = theta_d + (k/10.0)*pi
 			try
 				outcome = Mars_Project(theta_a, theta_d);
