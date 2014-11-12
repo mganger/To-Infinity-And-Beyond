e924a2e8d486fbb7fe6286cd0e65024fe299cba4
aredman <aredman314@gmail.com>
2014-11-11 20:02:55 -0500

    Fixed bug with calculating revolutions by simplifying code.

--- findDate.m
+++ findDate.m
@@ -1,20 +1,19 @@
 clear all; clc; close all; format long eng; more off; 
 addpath('helper_functions')
 
-file = fopen('file.txt', 'w')
+%file = fopen('file.txt', 'w')
 
-for i = 0:15
-	for j = 0:20
-		for k = 0:20
-			theta_d = (2*i)*pi + (j/10.0)*pi
-			theta_a = theta_d + (k/10.0)*pi
-			try
-				outcome = Mars_Project(theta_a, theta_d);
-				'--------------------------'
-				save('-append','file.txt','outcome')
-			catch
-				continue;
-			end
+for j = 0:320
+	for k = 0:20
+		theta_d = (j/10.0)*pi
+%		strcat(num2str(j/10),',',num2str(k))
+		theta_a = theta_d + (k/10.0)*pi
+		try
+			outcome = Mars_Project(theta_a, theta_d);
+		'--------------------------'
+			save('-append','file.txt','outcome')
+       		catch
+			continue;
 		end
 	end
 end
