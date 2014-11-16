578bc4317bb58fe89a5ec8ed0a6d32361fa7e3ae
aredman <aredman314@gmail.com>
2014-11-15 21:53:21 -0500

    prettied up the function in the analytic integration.

--- timeInt.m
+++ timeInt.m
@@ -11,9 +11,14 @@ function ret = timeInt(y,x)
 		return
 	end
 
-	first = x * sin(y) / ((x*x - 1)*(x*cos(y) + 1));
-	inside = (x-1) * tan(y/2) / sqrt(x*x - 1);
-	second = 2 * atanh(inside)/(x*x - 1).^(3/2.0);
+	first =      x * sin(y) 		...
+		/ ((x*x - 1)*(x*cos(y) + 1));
+
+	inside = (x-1) * tan(y/2) 		...
+		 / sqrt(x*x - 1);
+
+	second = 2 * atanh(inside) 		...
+		/ (x*x - 1).^(3/2.0);
 
 	if(y == pi) ret = real(first - second);
 	else ret = real( (first - second) ) + timeInt(pi,x).* 2 .*piCount - timeInt(pi,x).*2 .* floor(piCount/2);
