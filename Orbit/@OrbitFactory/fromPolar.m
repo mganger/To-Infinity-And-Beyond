ee9bc47dc96314e1f85a75322c8486cc6fe0f392
Michael Ganger <mganger747@gmail.com>
2014-11-16 22:59:18 -0500

    Two new functions to get the transfer orbit

---
+++ Orbit/@OrbitFactory/fromPolar.m
@@ -0,0 +1,14 @@
+%function to construct an orbit from two polar coordinates (r,theta*)
+%does not provide a reference angle
+function orb = fromPolarRef(fact, ci, cf)
+	r1 = ci(1); t1 = ci(2);
+	r2 = cf(1); t2 = cf(2);
+
+	ecc = orb.eccentricity = (r2 - r1) / (r1*cos(t1-refAngle) - r2*cos(t2-refAngle));
+	semiLatus = (r2 - r1) / (r1*cos(t1) - r2*cos(t2));
+
+	orb.semiMajor = semiLatus / (1 - ecc*ecc);
+	orb.centerMass = fact.centerMass;
+	orb.massName = fact.massName;
+	obj.bigG = 6.6738480e-11;
+end
---
+++ Orbit/@OrbitFactory/fromPolarRef.m
@@ -0,0 +1,18 @@
+function obj = fromPolar(orbFact, timeReq, ci, cf)
+	%optimize the polarRef function such that the amount of time between the two angles
+	%is the same as the time required
+
+	%get the orbit without the reference
+	tmpOrb = fromPolar(orbFact, ci, cf);
+
+	%make a function with the tmpOrb that shifts the reference angle and finds the timeDiff
+	func = @(ot) timeDiff(tmpOrb, ci(2) - ot, cf(2) - ot) - timeReq;
+
+	%optimize it; use fsolve
+	try
+		options = optimset('TolFun',1e-8,'TolX',1e-15,'MaxIter',1000);
+		obj.refAngle = fsolve(func, 0.2, options);
+	catch
+		error(strcat('Could not find the reference angle for the orbit around :' fact.massName));
+	end
+end
