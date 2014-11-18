1d9c52b0479714699ec1b7d015c022a91958e4fc
Michael Ganger <mganger747@gmail.com>
2014-11-17 20:29:34 -0500

    changed graph function so that it plots the offset angle

--- graph.m
+++ graph.m
@@ -1,23 +1,22 @@
 function ret = graph(orbit,theta1,theta2,figureNum)
 
-try 
-figureNum == pi
-catch
-figureNum = 1;
+try figureNum == pi
+catch figureNum = 1;
 end
 
-try
-	domain = linspace(theta1,theta2,1000);
-catch
-	domain = linspace(0,2*pi,1000);
+try domain = linspace(theta1,theta2,1000);
+catch domain = linspace(0,2*pi,1000);
 end
 	
 for i = 1:length(domain)
 	rad(i) = radiusAbs(orbit,domain(i));
-	rad2(i) = radiusAbs(orbit,domain(i));
 end
 
+%make a polar plot of the semimajor axis
+xdomain = linspace(-1,1,10).*orbit.semiMajor;
+
 figure(figureNum)
-	hold on
+	hold on;
 	polar(domain,rad);
+	plot(xdomain, xdomain.*tan(orbit.refAngle));
 end
