ae61cf718c8e1c44af67b1992bba641e3c9e83d4
aredman <aredman314@gmail.com>
2014-11-18 13:59:31 -0500

    Created function to calculate Hohmann Transfers takes two orbits and an omega(optional)

---
+++ Orbit/@OrbitFactory/hohmannTransfer.m
@@ -0,0 +1,18 @@
+function ret = hohmannTransfer(orbit1,orbit2,omega)
+	try omega == 2;
+		ret.refAngle = omega;
+	catch 
+		ret.refAngle = 0;
+	end
+
+	G = 6.6738480e-11;
+	rmin = orbit2.rmin;
+	rmax = orbit1.rmin;
+	vmin = orbit2.vmin;
+	vmax = orbit1.vmin;
+
+	ret.alpha = 2*rmax*rmin/(rmax+rmin);
+	ret.eccentricity = ((ret.alpha-rmin)/rmin);
+	ret.semiMajor = ret.alpha/(1-ret.eccentricity.^2)
+	ret.bigG = G;
+end
