f4a91cfbb6d99de4204da8e1e2638c7db8e6cd50
aredman <aredman314@gmail.com>
2014-11-17 21:06:05 -0500

    Added alpha, minimum radius, and minimum velocity to orbital objects.

--- Orbit/@Orbit/Orbit.m
+++ Orbit/@Orbit/Orbit.m
@@ -1,9 +1,12 @@
 function obj = Orbit(a,e,w,m,name)
+	obj.massName = name;
+	obj.bigG = 6.6738480e-11;
+
 	obj.semiMajor = a;
 	obj.refAngle = w;
 	obj.eccentricity = e;
 	obj.centerMass = m;
-
-	obj.massName = name;
-	obj.bigG = 6.6738480e-11;
+	obj.alpha = a*(1-e.^2);
+	obj.rmin = obj.alpha/(1+e);
+	obj.vmin = sqrt(obj.bigG*2*m*(1/obj.rmin-1/(2*a)));
 end
--- Orbit/@OrbitFactory/fromHYP.m
+++ Orbit/@OrbitFactory/fromHYP.m
@@ -1,26 +1,10 @@
 function o = fromHYP(obj,vinf,rmin,angleOffset)
 	G = 6.6738480e-11;
-	obj.centerMass
-	G
-	vinf
-	rmin
-	angleOffset
-
-%	o.eccentricity = -(rmin*vinf*vinf+obj.centerMass*G)/(rmin*vinf*vinf);
-%	o.semiMajor = (-obj.centerMass*G)/(vinf*vinf)
-%	o.semiMajor = rmin/(1-o.eccentricity)
-	vmin = sqrt(vinf.^2+2*G*obj.centerMass/rmin)
-%	o.semiMajor = G*M/(2*(G*M/rmin)-(vmin).^2)
-	o.semiMajor = -G*obj.centerMass/vinf.^2
-	o.eccentricity = 1-rmin/o.semiMajor
-	o.alpha = rmin + rmin * o.eccentricity
+	o.vmin = sqrt(vinf.^2+2*G*obj.centerMass/rmin);
+	o.semiMajor = -G*obj.centerMass/vinf.^2;
+	o.eccentricity = 1-rmin/o.semiMajor;
+	o.alpha = rmin + rmin * o.eccentricity;
 	o.refAngle = angleOffset;
 	o.centerMass = obj.centerMass;
 	o.bigG = G;
 end
-
-%Things we need:
-%center mass
-%eccentricity
-%semi major axis
-%angleOffset
