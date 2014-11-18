5d2241146f9093433a94d7483fe3f40e122d8b42
aredman <aredman314@gmail.com>
2014-11-17 22:24:46 -0500

    allowed to change size on pointGraph

--- pointGraph.m
+++ pointGraph.m
@@ -1,4 +1,8 @@
-function ret = pointGraph(orbit,angle)
+function ret = pointGraph(orbit,angle,size)
+	try size == 2
+	scatter(radiusAbs(orbit,angle)*cos(angle),radiusAbs(orbit,angle)*sin(angle),size);
+	catch
 	hold on;
 	scatter(radiusAbs(orbit,angle)*cos(angle),radiusAbs(orbit,angle)*sin(angle),30);
+	end
 end
