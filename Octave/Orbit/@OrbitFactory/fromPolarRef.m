d28a7d9795a71f947eb52c2640711ed455083339
Michael Ganger <mganger747@gmail.com>
2014-11-21 15:10:57 -0500

    added comments to fromPolarRef

--- Octave/Orbit/@OrbitFactory/fromPolarRef.m
+++ Octave/Orbit/@OrbitFactory/fromPolarRef.m
@@ -1,3 +1,8 @@
+%This function is use to find a transfer orbit between two given polar
+%coordinates, with a transfer time eqivalent to the one provided. If graph is
+%equal to one, it will also plot the graph that fsolve is finding the root of.
+%This generally finds the correct orbit; the only major problem results from
+%fsolve trying imaginary values.
 function obj, tDiff = fromPolarRef(orbFact, timeReq, ci, cf, graph)
 	%optimize the polarRef function such that the amount of time between the two angles
 	%is the same as the time required
@@ -29,6 +34,10 @@ function obj, tDiff = fromPolarRef(orbFact, timeReq, ci, cf, graph)
 	end
 end
 
+%This function calculates an orbit between two polar coordinates given a
+%reference angle, and returns the amount of time that it takes the orbiting
+%body to travel between two points. This is the function that the above
+%function optimizes
 function tDiff, orb = tDiffFunction(orbFact, ot, ci, cf)
 	r1 = ci(1); t1 = ci(2) - ot;
 	r2 = cf(1); t2 = cf(2) - ot;
