5de26ff28b87740125885206a6d7d45023c678fd
Michael Ganger <mganger747@gmail.com>
2014-11-21 15:04:38 -0500

    added comments to the top of the orbit factory constructor

--- Octave/Orbit/@OrbitFactory/OrbitFactory.m
+++ Octave/Orbit/@OrbitFactory/OrbitFactory.m
@@ -1,3 +1,7 @@
+%Constructor for the Orbit factory. Although the term "factory" is used
+%throughout the code, what it really represents is the large mass at the center
+%of orbits (Sun, Earth, Mars). "Factory" is just used to keep the factory
+%pattern in mind
 function obj = OrbitFactory(mass,name)
 	obj.centerMass = mass;
 	obj.massName = name;
