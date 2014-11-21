58844ccb7176db793a58d9fcf397c116e25edbc4
Michael Ganger <mganger747@gmail.com>
2014-11-21 15:01:09 -0500

    added comments fromAE

--- Octave/Orbit/@OrbitFactory/fromAE.m
+++ Octave/Orbit/@OrbitFactory/fromAE.m
@@ -1,3 +1,6 @@
+%Simplest construction of orbits from the orbital parameters. Takes the factory
+%(body), semimajor axis, eccentricity, and angle of offset from the reference
+%angle (omegaT)
 function o = fromAE(obj,semi, eccent, angleOffset)
 	o = Orbit(semi, eccent, angleOffset, obj.centerMass, obj.massName);
 end
