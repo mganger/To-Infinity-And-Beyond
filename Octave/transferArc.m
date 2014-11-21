464d5863aac1e7f2629f343fda9e409685786ea0
Michael Ganger <mganger747@gmail.com>
2014-11-21 14:54:51 -0500

    added comments to the trnasfer arc function

--- Octave/transferArc.m
+++ Octave/transferArc.m
@@ -1,17 +1,31 @@
+%This is a lengthy function that finds the transfer orbit between two different
+%points. The basic process is as such:
+%	1 - find the amount of time for the second body to reach the position of the
+%	    first body
+%	2 - start constructing an orbit out of the two polar coordinates
+%	3 - start optimizing the reference angle (omegaT) so that the amount of
+%	    to complete the transfer orbit is the same as the amount of time for
+%	    the second planet to reach the given angle of arrival (thetaA)
 function transOrb, map, reqTime, transTime = transferArc(factory, orbit1, perTime1, orbit2, perTime2,  thetaD, thetaA, filename, quiet, plot)
 	try quiet == 1; catch quiet = 0; end;
 	try plot == 1; catch plot = 0; end;
+	%create a struct to pass around different conditions at given points in
+	%the orbit time. Essentially, this acts as a way to keep all of the
+	%information relevant to the transfer orbit in one location.
 	map = struct;
 
-	%periapsis of second minus periapsis of first, in seconds
+	%Periapsis of second minus periapsis of first, in seconds. This is used
+	%to align the position of the orbits at a given date
 	periTimeDiff = (perTime1 - perTime2)*24*3600;
-	%angle of.second in its orbit at t = 0
+	%Angle of second in its orbit at t = 0
 	map.init.time =  0;
 	map.init.first = 0;
 	map.init.second = angSolve(orbit2, 0, periTimeDiff);
 
 
-	%arrival and departure angles (user inputed)
+	%Adjust the arrival and departure so that we always leave in the past
+	%and arrive in the future. Then, set the reference angles of both
+	%positons in the struct
 	while(thetaD < orbit1.refAngle) thetaD += 2*pi; end;
 	while(thetaA < thetaD)          thetaA += 2*pi; end;
 	map.depart.ref  = thetaD;
@@ -20,11 +34,11 @@ function transOrb, map, reqTime, transTime = transferArc(factory, orbit1, perTim
 	printf('thetaA: %f\n', thetaA);
 
 
-	%find the eqivalent.first and.second angles for above reference angles
+	%Find the eqivalent first and second angles for above reference angles
 	map.depart.first =   relAngle(thetaD, orbit1);
 	map.arrival.second = relAngle(thetaA, orbit2);
 
-	%find the time that the.first is at the desired depart angle
+	%Find the time that the first is at the desired depart angle
 	map.depart.time = timeDiff(orbit1, 0, map.depart.first);
 
 	%find second's location at the departure angle (for the first)
@@ -54,6 +68,7 @@ function transOrb, map, reqTime, transTime = transferArc(factory, orbit1, perTim
 	map.depart.transfer  = relAngle(map.depart.ref, transOrb);
 	map.arrival.transfer = relAngle(map.arrival.ref, transOrb);
 
+	%If the plot variable is set, print out the graph to the filename
 	if(plot > 0)
 		figure(plot, 'visible', 'off')
 			%second and first location at departure
